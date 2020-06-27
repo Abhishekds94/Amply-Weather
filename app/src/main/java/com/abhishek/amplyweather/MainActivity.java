df342bc91dc6614130d8073c661b0694de34fa3d
Abhishek D S <abhishekds94@gmail.com>
2020-06-27 16:03:07 -0500

    Splash UI set based on system time

--- app/src/main/java/com/abhishek/amplyweather/MainActivity.java
+++ app/src/main/java/com/abhishek/amplyweather/MainActivity.java
@@ -3,10 +3,19 @@ package com.abhishek.amplyweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout splashMorning;
+    RelativeLayout splashNoon;
+    RelativeLayout splashNight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +27,33 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+        Toast.makeText(this, "Time-"+timeOfDay, Toast.LENGTH_SHORT).show();
+        splashMorning = (RelativeLayout) findViewById(R.id.splash_morning);
+        splashNoon = (RelativeLayout) findViewById(R.id.splash_noon);
+        splashNight = (RelativeLayout) findViewById(R.id.splash_night);
+
+        if(timeOfDay >= 6 && timeOfDay < 14){
+            if (splashMorning.getVisibility() != View.INVISIBLE) {
+                splashMorning.setVisibility(View.VISIBLE);
+                splashNoon.setVisibility(View.INVISIBLE);
+                splashNight.setVisibility(View.INVISIBLE);
+            }
+        }else if(timeOfDay >= 14 && timeOfDay < 19){
+            if (splashNoon.getVisibility() != View.INVISIBLE) {
+                splashNoon.setVisibility(View.VISIBLE);
+                splashMorning.setVisibility(View.INVISIBLE);
+                splashNight.setVisibility(View.INVISIBLE);
+            }
+        }else if(timeOfDay >= 19 && timeOfDay < 6){
+            if (splashNight.getVisibility() == View.INVISIBLE) {
+                splashNight.setVisibility(View.VISIBLE);
+                splashNoon.setVisibility(View.INVISIBLE);
+                splashMorning.setVisibility(View.INVISIBLE);
+            }
+        }
+
     }
 }
