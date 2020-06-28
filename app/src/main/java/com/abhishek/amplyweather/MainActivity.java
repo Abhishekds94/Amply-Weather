37d41f25dfbef665b1b43de8090e248f8a0a54d0
Abhishek D S <abhishekds94@gmail.com>
2020-06-27 20:16:34 -0500

    Added InternetCheck with UI navigation

--- app/src/main/java/com/abhishek/amplyweather/MainActivity.java
+++ app/src/main/java/com/abhishek/amplyweather/MainActivity.java
@@ -2,15 +2,23 @@ package com.abhishek.amplyweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -67,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 splashMorning.setVisibility(View.INVISIBLE);
             }
         }
-        navigate();
+        checkInternetAvailibility();
     }
 
     //Navigate to Home Activity after running the splash screen for 6 seconds -> time is based on the Animation speed for all 3 UIs
@@ -81,6 +89,74 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
+    }
+
+    public void checkInternetAvailibility() {
+        if (isInternetAvailable()) {
+            new IsInternetActive().execute();
+            navigate();
+        } else {
+            Intent intent = new Intent(MainActivity.this, NoInternet.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    public boolean isInternetAvailable() {
+        try {
+
+            ConnectivityManager connectivityManager
+                    = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+        } catch (Exception e) {
+
+            Log.e("isInternetAvailable:", e.toString());
+            return false;
+        }
+    }
+
+    class IsInternetActive extends AsyncTask<Void, Void, String> {
+        InputStream is = null;
+        String json = "Fail";
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                URL strUrl = new URL("http://icons.iconarchive.com/icons/designbolts/handstitch-social/24/Android-icon.png");
+                //Icon for testing the server
+
+                URLConnection connection = strUrl.openConnection();
+                connection.setDoOutput(true);
+                is = connection.getInputStream();
+                json = "Success";
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                json = "Fail";
+            }
+            return json;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                if (result.equals("Fail")) {
+                    Log.e("Internet Not Active", "Internet Not Active");
+                } else {
+                    Log.e("Internet Active", "Internet Active" + result);
+                }
+            } else {
+                Log.e("Internet Not Active", "Internet Not Active");
+            }
+        }
+
+        @Override
+        protected void onPreExecute() {
+            Log.e("Validating", "Validating Internet");
+            super.onPreExecute();
+        }
     }
 
 }
